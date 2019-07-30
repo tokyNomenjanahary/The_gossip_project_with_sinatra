521ba1af7c33b9223f7a973c1ecfebb477ae998a
tokynomenaja <nomenjatoky21@gmail.com>
2019-07-30 07:37:57 +0300

    insert folder which contains the file of the_gossip_project

---
+++ lib/controller.rb
@@ -0,0 +1,27 @@
+require 'gossip'
+class ApplicationController < Sinatra::Base
+
+	get '/' do
+		erb :index, locals: {gossips: Gossip.all}
+	end
+
+	get '/gossips/new/' do
+		erb :new_gossip
+	end
+	post '/gossips/new/' do
+		erb :new_gossip
+		Gossip.new(params["gossip_author"], params["gossip_content"]).save
+	  puts "Salut, je suis dans le serveur"
+	  puts "Ceci est le contenu du hash params : #{params}"
+	  puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
+	  puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
+	  puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
+
+	  redirect '/'
+	end
+
+	get '/gossips/:id/' do
+		erb :show, locals: {gossip: Gossip.find(params['id']), i: params['id']}
+	end
+
+end
---
+++ lib/gossip.rb
@@ -0,0 +1,30 @@
+require 'csv'
+
+class Gossip
+	attr_accessor :author ,:content
+
+	def initialize(author, content)
+		@author = author
+		@content = content
+
+	end
+
+	def save
+		CSV.open("db/gossip.csv", "a+") do |csv|
+			csv << [@author, @content]
+		end		
+	end
+	def self.all
+		all_gossips = []
+		CSV.read("db/gossip.csv").each do |csv_line|
+			all_gossips << Gossip.new(csv_line[0], csv_line[1])
+		end
+		return all_gossips
+	end
+
+	def self.find(id)
+		id = id.to_i
+		table = CSV.read("db/gossip.csv")
+		return table[id]
+	end
+end
